20722adee66739916eb70e8b780f93bb031b2933
Marco Braak <mbraak@ridethepony.nl>
2013-10-14 16:58:16 +0200

    Use sqlite for test settings

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -5,11 +5,11 @@ TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'fiber',                # Or path to database file if using sqlite3.
-        'USER': 'root',                              # Not used with sqlite3.
-        'PASSWORD': 'root1',                          # Not used with sqlite3.
-        'HOST': '127.0.0.1',                              # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '13306',                              # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'testproject_db',                # Or path to database file if using sqlite3.
+        'USER': '',                              # Not used with sqlite3.
+        'PASSWORD': '',                          # Not used with sqlite3.
+        'HOST': '',                              # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                              # Set to empty string for default. Not used with sqlite3.
     }
 }
