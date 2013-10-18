b9b75ec4e2d519662d6d196dc6cf78e6c7c47bc5
Marco Braak <mbraak@ridethepony.nl>
2013-10-18 15:09:59 +0200

    Users cannot access the fiber login page

--- fiber/fiber_admin/__init__.py
+++ fiber/fiber_admin/__init__.py
@@ -1,6 +1,7 @@
 # MPTTModelAdmin is unused, but should stay since its import from here
 # has been referenced in documentation.
 from django.contrib import admin
+from django.http.response import HttpResponseForbidden
 from .options import ModelAdmin, MPTTModelAdmin
 
 
@@ -11,5 +12,8 @@ class FiberAdminSite(admin.AdminSite):
             admin_class = ModelAdmin
         return super(FiberAdminSite, self).register(model_or_iterable, admin_class=admin_class, **options)
 
+    def login(self, request, extra_context=None):
+        # The user is not allowed to login using this url
+        return HttpResponseForbidden()
 
 site = FiberAdminSite(name='fiber_admin')
